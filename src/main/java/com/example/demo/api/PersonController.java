ff9cd61c65a449151b2096c6e7ba90a924f9c7ef
Nahashon Njenga <nahaseannjenga@gmail.com>
2019-05-07 23:53:24 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,49 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/api/person")
+@RestController
+public class PersonController {
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.selectPersonById(id).orElse(null);
+    }
+
+    @PutMapping(path = "{id}")
+    public int updatePersonById(@PathVariable("id") UUID id,
+                                @Valid @NotNull @RequestBody Person person) {
+        return personService.updatePersonById(id, person);
+    }
+
+    @DeleteMapping(path="{id}")
+    public int deletePersonById(@PathVariable("id") UUID id) {
+        return personService.deletePerson(id);
+    }
+
+}
